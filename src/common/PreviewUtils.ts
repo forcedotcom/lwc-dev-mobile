2d6d6821954b31a81ec63196116c1175717de3d4
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2020-09-28 11:47:35 -0700

    fix linting errors

--- src/common/PreviewUtils.ts
+++ src/common/PreviewUtils.ts
@@ -8,17 +8,17 @@ import Ajv from 'ajv';
 import fs from 'fs';
 import { PreviewConfigFile } from './PreviewConfigFile';
 
+const NAMESPACE = 'com.salesforce.mobile-tooling';
+
 export interface ValidationResult {
     errorMessage: string | null;
     passed: boolean;
 }
 
 export class PreviewUtils {
-    private static NAMESPACE = 'com.salesforce.mobile-tooling';
-
     public static BROWSER_TARGET_APP = 'browser';
-    public static COMPONENT_NAME_ARG_PREFIX = `${PreviewUtils.NAMESPACE}.componentname`;
-    public static PROJECT_DIR_ARG_PREFIX = `${PreviewUtils.NAMESPACE}.projectdir`;
+    public static COMPONENT_NAME_ARG_PREFIX = `${NAMESPACE}.componentname`;
+    public static PROJECT_DIR_ARG_PREFIX = `${NAMESPACE}.projectdir`;
 
     public static isTargetingBrowser(targetApp: string): boolean {
         return (
