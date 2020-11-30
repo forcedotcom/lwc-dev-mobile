c5fde2bb4288f0bdcd4c829a74485fba048cf81a
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2020-11-30 12:32:35 -0800

    clean up references to personal info

--- samples/apps/ios/LwcTestApp/LwcTestApp/AppDelegate.swift
+++ samples/apps/ios/LwcTestApp/LwcTestApp/AppDelegate.swift
@@ -2,8 +2,9 @@
 //  AppDelegate.swift
 //  LwcTestApp
 //
-//  Created by Meisam Seyed Aliroteh on 8/12/20.
-//  Copyright © 2020 Meisam Seyed Aliroteh. All rights reserved.
+//  Copyright (c) 2020, salesforce.com, inc. All rights reserved.
+//  SPDX-License-Identifier: MIT
+//  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
 //
 
 import UIKit
