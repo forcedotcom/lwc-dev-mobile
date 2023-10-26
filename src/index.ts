acab039f9b8e94a71f77fc1c3914bfdd045b8156
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2023-10-26 11:47:49 -0700

    re-export everything

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,9 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-export default {};
+
+export * from './cli/commands/force/lightning/local/device/create';
+export * from './cli/commands/force/lightning/local/device/list';
+export * from './cli/commands/force/lightning/local/device/start';
+
+export * from './cli/commands/force/lightning/lwc/preview';
